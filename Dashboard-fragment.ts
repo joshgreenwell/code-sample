8dcd3af279a9d4a671e8e9450b52638dd4244f7f
Josh Greenwell <31043400+joshgreenwell@users.noreply.github.com>
2020-02-17 11:57:29 -0800

    Create Dashboard-fragment.ts

---
+++ Dashboard-fragment.ts
@@ -0,0 +1,32 @@
+useEffect(() => {
+    const getUserData = async () => {
+      const data = await getUserThoughtsRequest(user.id)
+      const thoughts: Thought[] = []
+      const spiritCards: Thought[] = []
+
+      data.forEach((thought: Thought) => {
+        if (thought.isSpiritCard) {
+          spiritCards.push(thought)
+        } else {
+          thoughts.push(thought)
+        }
+      })
+      
+      dispatch({ type: 'setUserThoughts', thoughts })
+      dispatch({ type: 'setUserSpiritCards', spiritCards })
+
+      const entries = await getJournalEntriesRequest(user.id)
+
+      dispatch({ type: 'setJournalEntries', entries })
+    }
+
+    const getSharedData = async () => {
+      const data = await getSharedInfo(user.id)
+      dispatch({ type: 'setShared', shared: data })
+    }
+    
+    if (user.id) {
+      getUserData()
+      getSharedData()
+    }
+  }, [user])
